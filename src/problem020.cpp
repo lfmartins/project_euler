e2305c7cc612335daaecfb4a3652ee1fc0f1e5e0
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-07-28 13:00:28 -0400

    fix problem numbers (19 and 20)

--- src/problem020.cpp
+++ src/problem020.cpp
@@ -1,9 +1,29 @@
-// problem020.cpp
+// problem019.cpp
 #include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <tools.h>
 
+using boost::multiprecision::cpp_int;
+
 int main() {
-    std::cout << "Solving Project Euler Problem 020" << std::endl;
+    std::cout << "Solving Project Euler Problem 019" << std::endl;
+
+    ull n = 20;
+    cpp_int factorial = 1;
+
+    for (ull i = 2; i <= n; ++i) {
+        factorial *= i;
+    }
+
+    std::cout << factorial << std::endl;
+
+    cpp_int digit_sum = 0;
+    while (factorial > 0) {
+        digit_sum += factorial % 10;
+        factorial /= 10;
+    }
+
+    std::cout << digit_sum << std::endl;
 
     return 0;
 }
