76fc9cea2fae9b46e7912877430454dcb56b7390
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-07-28 12:47:35 -0400

    add problem 20 the right way

---
+++ src/problem020.cpp
@@ -0,0 +1,9 @@
+// problem020.cpp
+#include <iostream>
+#include <tools.h>
+
+int main() {
+    std::cout << "Solving Project Euler Problem 020" << std::endl;
+
+    return 0;
+}
--- src/problem20.cpp
+++
@@ -1,9 +0,0 @@
-// problem20.cpp
-#include <iostream>
-#include <tools.h>
-
-int main() {
-    std::cout << "Solving Project Euler Problem 20" << std::endl;
-
-    return 0;
-}
