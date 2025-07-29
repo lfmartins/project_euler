48aa6dc7e5eac5d4bcb40c940d27f96bb3ca7d05
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-07-28 23:30:35 -0400

    solve problem 022

--- src/problem022.cpp
+++ src/problem022.cpp
@@ -1,9 +1,49 @@
 // problem022.cpp
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
 #include <tools.h>
+
+ull alpha_score(std::string word) {
+    ull score = 0;
+    for (char c: word) {
+        score += c - 'A' + 1;
+    }
+    
+    return score;
+}
 
 int main() {
     std::cout << "Solving Project Euler Problem 022" << std::endl;
+
+    std::ifstream file("data/0022_names.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "Cannot open file" << std::endl;
+        return 1;
+    }
+
+    std::string contents(
+            (std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>());
+    std::stringstream ss(contents);
+    std::string name;
+    std::vector<std::string> names;
+
+    while(std::getline(ss, name, ',')) {
+        names.push_back(name.substr(1, name.size() - 2));
+    }
+
+    std::sort(names.begin(), names.end());
+
+    ull sum = 0;
+    for (size_t i = 0; i < names.size(); ++i) {
+        sum += (i + 1) * alpha_score(names[i]);
+    }
+
+    std::cout << sum << std::endl;
 
     return 0;
 }
