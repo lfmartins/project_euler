14f530095138129a3885639b25f685e90b06922c
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-07-30 21:43:50 -0400

    work on problem 25

--- src/problem025.cpp
+++ src/problem025.cpp
@@ -1,9 +1,20 @@
 // problem025.cpp
 #include <iostream>
+#include <cmath>
 #include <tools.h>
 
 int main() {
     std::cout << "Solving Project Euler Problem 025" << std::endl;
 
+    double phi = (1 + std::sqrt(5)) * 0.5;
+    double psi = (1 - std::sqrt(5)) * 0.5;
+    double c = 1 / sqrt(5);
+
+    ull ndigits = 1000;
+
+    ull n = static_cast<ull>(std::floor((ndigits - std::log10(c)) / std::log10(phi)));
+
+    std::cout << n << std::endl;
+    
     return 0;
 }
