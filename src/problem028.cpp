fc6666ad021c8c3ccd266b04fbdd326c18e2e353
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-08-05 16:45:11 -0400

    solve problem 28

--- src/problem028.cpp
+++ src/problem028.cpp
@@ -5,5 +5,13 @@
 int main() {
     std::cout << "Solving Project Euler Problem 028" << std::endl;
 
+    ull nmax = 1001;
+    ull sum = 1;
+    for (ull n = 3; n <= nmax; n += 2) {
+        ull sum_corners = 2 * (n * (2 * n - 3) + 3);
+        sum += sum_corners;
+    }
+    std::cout << sum << std::endl;
+
     return 0;
 }
