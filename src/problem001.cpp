e33e5c775db0ed01b5fae07e6340f097c31438bf
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-08-06 03:13:25 -0400

    create problem030.cpp

--- src/problem001.cpp
+++ src/problem001.cpp
@@ -1,19 +1,9 @@
 // problem001.cpp
-
 #include <iostream>
 #include <tools.h>
 
 int main() {
     std::cout << "Solving Project Euler Problem 001" << std::endl;
-    
-    ull sum = 0;
 
-    for (ull i = 0; i < 1000; ++i) {
-        if (i % 3 == 0 || i % 5 == 0)
-           sum += i;
-    }
-
-    std::cout << sum << std::endl;
-
-    return 0; 
+    return 0;
 }
---
+++ src/problem030.cpp
@@ -0,0 +1,9 @@
+// problem030.cpp
+#include <iostream>
+#include <tools.h>
+
+int main() {
+    std::cout << "Solving Project Euler Problem 030" << std::endl;
+
+    return 0;
+}
