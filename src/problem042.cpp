8d9fae87835007aa97aeeb466888e5cde2262cb9
Luiz Felipe Martins <luizfelipe.martins@gmail.com>
2025-08-07 13:46:04 -0400

    solve problem 42

---
+++ src/problem042.cpp
@@ -0,0 +1,44 @@
+// problem042.cpp
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include <tools.h>
+
+int main() {
+    std::cout << "Solving Project Euler Problem 042" << std::endl;
+
+    std::string file_name("data/0042_words.txt");
+    std::ifstream file(file_name);
+
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file " << file_name;
+        return 1;
+    }
+    
+    std::string line;
+    ull count = 0;
+    while(std::getline(file, line, ',')) {
+        std::string word = line.substr(1, line.size()-2);
+        ull score = 0;
+        for (char c: word) {
+            score += c - 'A' + 1;
+        }
+//        std::cout << "|" << word << "| " << score << std::endl;
+        for (ull n = 1;; ++n) {
+            ull t = n * (n + 1) / 2;
+            if (t < score) {
+                continue;
+            }
+            if (t == score) {
+                ++count;
+            }
+            break;
+        }
+    }
+
+    std::cout << "Solution: " << count << std::endl;
+
+    return 0;
+}
